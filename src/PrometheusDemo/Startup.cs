ed6ebca428f43392084ed8b7af807e7503417182
SamZhang <samzhang80@hotmail.com>
2020-11-08 23:56:46 +0800

    Initial project.

---
+++ src/PrometheusDemo/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prometheus;
+using Prometheus.HttpMetrics;
+
+namespace PrometheusDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpMetrics(opt =>
+            {
+                opt.AddRouteParameter(new HttpRouteParameterMapping("appName", "app_name"));
+                opt.InProgress.Enabled = true;
+                opt.RequestCount.Enabled = true;
+                opt.RequestDuration.Enabled = true;
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UsePathBase("/ShopApi");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                endpoints.MapMetrics();
+            });
+        }
+    }
+}
